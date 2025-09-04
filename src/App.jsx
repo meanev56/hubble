01e59f6db006be5b3c6e806259f2be06bd5f747d
meanev56 <emekanjoku86@gmail.com>
2025-09-05 00:49:52 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333"
+
+  }
+}
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+    <Header />
+      <Container>
+        <h1>Hello, World!</h1>
+      
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
